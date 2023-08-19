b4935cd4e4f4b775d669b866e8017b3eec4e87b7
Developerraju23 <rajukumaraju.rk@gmail.com>
2023-08-19 10:49:09 +0600

    first material ui install

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import { Typography } from '@mui/material'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+        <Typography variant='h1'>
+            Hello
+        </Typography>
+    </>
+  )
+}
+
+export default App
